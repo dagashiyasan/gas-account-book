69332957d1d32a2a2ba7f08a0a38b638ee4998b0
dagashiyasan <yukiwork.125@gmail.com>
2021-02-27 13:45:58 +0900

    account-book commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <!-- ツールバー -->
+    <v-app-bar app color="green" dark>
+      <!-- タイトル -->
+      <v-toolbar-title>{{ appName }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- テーブルアイコンのボタン -->
+      <v-btn icon to="/"> <!-- クリックで "/" へ移動する -->
+        <v-icon>mdi-file-table-outline</v-icon>
+      </v-btn>
+      <!-- 歯車アイコンのボタン -->
+      <v-btn icon to="/settings"> <!-- クリックで "/settings" へ移動する -->
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <!-- メインコンテンツ -->
+    <v-main>
+      <v-container fluid>
+        <!-- router-view の中身がパスによって切り替わる -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'App',
+
+  computed: mapState({
+    appName: state => state.settings.appName
+  }),
+  // Appインスタンス生成前に一度だけ実行されます
+  beforeCreate () {
+    this.$store.dispatch('loadSettings')
+  }
+}
+
+</script>
